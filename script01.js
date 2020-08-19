55212a45d197b6a51c6a03a25bf251ab6532eee2
jorgebarbosat <62555995+jorgebarbosat@users.noreply.github.com>
2020-08-19 10:47:22 -0300

    .

--- script01.js
+++ script01.js
@@ -1,5 +1,9 @@
 var http = require('http');
 
-http.createServer().listen(8080);
+http.createServer(function(req, res) {
+    res.writeHead(200, {'Content-Type':'text/plain; charset=UTF-8'});
+    res.write('Desenvolvimento de Aplicações Distribuidas e Moveis');
+    res.end();
+}).listen(8080);
 
 console.log('Servidor Iniciado na porta 8080. Pressione Ctrl + C para encerrar');
