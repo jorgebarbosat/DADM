e1993efdb283b3c00da6cfb4d21a41ad3387cf15
jorgebarbosat <62555995+jorgebarbosat@users.noreply.github.com>
2020-08-19 11:58:05 -0300

    .

---
+++ script02.js
@@ -0,0 +1,16 @@
+var http = require('http');
+
+http.createServer(function(req, res) {
+    res.writeHead(200, {'Content-Type':'text/html'})
+    res.write('<html>');
+    res.write('<head>');
+    res.write('<title>Aula de Node.js</title>');
+    res.write('</head>');
+    res.write('<body>');
+    res.write('<h1>Desenvolvimento de Aplicações Web</h1>');
+    res.write('<p>Aprender programação é construir o futuro.</p>');
+    res.write('</body>');
+    res.write('</html>');
+}).listen(8080);
+
+console.log('Servidor Iniciado na porta 8080. Pressione Ctrl + C para encerrar');
