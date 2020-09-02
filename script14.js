7e4116b921a5e7bf32bc3e6cf84514d8bedccd46
jorgebarbosat <62555995+jorgebarbosat@users.noreply.github.com>
2020-09-02 11:30:24 -0300

    .

---
+++ script14.js
@@ -0,0 +1,24 @@
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+
+http.createServer(
+    function(req, res) {
+        var dados = url.parse(req.url, true).query;
+        var nome = dados.nome;
+        var sobrenome = dados.sobrenome;
+        var nc = nome + '' + sobrenome + '\n';
+
+        fs.appendFile('listaNomes.txt', nc, function(err){
+            if(err)
+                throw err;
+        })
+
+        res.writeHead(200, {'Content-Type':'text/html; charset=UTF-8'});
+        res.write('Nome: ' + nome + '<br>');
+        res.write('Sobrenome: ' + sobrenome);
+        res.end();
+    }
+).listen(8080);
+
+console.log('Servidor iniciado na porta 8080. Pressione Ctrl + C para encerrar.');
